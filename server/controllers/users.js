19de3430fe199269dda756ad78a76982f397f961
YuraBorsch <yura_borsch@ukr.net>
2017-10-31 23:31:07 +0200

    Add support of unique emails.

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,52 +1,58 @@
-	const User = require('../models').User;
+const Sequelize = require('sequelize');
+const User = require('../models').User;
 
 
-	module.exports = {
-	  create(req, res) {
-	    return User
-	      .create({
-	        email: req.body.email,
-	        password: req.body.password,
-	      })
-	      .then(user => res.status(201).send(user))
-	      .catch(error => res.status(400).send(error));
-	  },
-	  getAll(req, res) {
-	    return User
-	      .findAll()
-	      .then(users => res.status(200).send(users))
-	      .catch(error => res.status(400).send(error));
-	  },
-	  get(req, res) {
-	    return User
-	      .findById(req.params.userId)
-	      .then(user => {
-	        if (!user) {
-	          return res.status(404).send({
-	            message: 'User Not Found',
-	          });
-	        }
-	        return res.status(200).send(user);
-	      })
-	      .catch(error => res.status(400).send(error));
-	  },
-	  update(req, res) {
-  		return User
-    	  .findById(req.params.userId)
-          .then(user => {
-            if (!user) {
-              return res.status(404).send({
-                message: 'User Not Found',
-              });
-            }
-            return user
-              .update({
-                email: req.body.email || user.email,
-                password: req.body.password || user.password,
-               })
-              .then(() => res.status(200).send(user))  // Send back the updated todo.
-              .catch((error) => res.status(400).send(error));
-          })
-          .catch((error) => res.status(400).send(error));
-      },
-	};
+
+module.exports = {
+    create(req, res) {
+        return User
+            .create({
+                email: req.body.email,
+                password: req.body.password,
+            })
+            .then(user => res.status(201).send(user))
+            .catch(Sequelize.ValidationError, function (err) {
+                // respond with validation errors
+                return res.status(422).send(err.errors);
+            })
+            .catch(error => res.status(400).send(error));
+    },
+    getAll(req, res) {
+        return User
+            .findAll()
+            .then(users => res.status(200).send(users))
+            .catch(error => res.status(400).send(error));
+    },
+    get(req, res) {
+        return User
+            .findById(req.params.userId)
+            .then(user => {
+                if (!user) {
+                    return res.status(404).send({
+                        message: 'User Not Found',
+                    });
+                }
+                return res.status(200).send(user);
+            })
+            .catch(error => res.status(400).send(error));
+    },
+    update(req, res) {
+        return User
+            .findById(req.params.userId)
+            .then(user => {
+                if (!user) {
+                    return res.status(404).send({
+                        message: 'User Not Found',
+                    });
+                }
+                return user
+                    .update({
+                        email: req.body.email || user.email,
+                        password: req.body.password || user.password,
+                    })
+                    .then(() => res.status(200).send(user))  // Send back the updated todo.
+                    .catch((error) => res.status(400).send(error));
+            })
+            .catch((error) => res.status(400).send(error));
+    },
+};
--- server/models/user.js
+++ server/models/user.js
@@ -3,62 +3,75 @@
 const bcrypt = require("bcrypt");
 
 module.exports = (sequelize, DataTypes) => {
-  var User = sequelize.define('User', {
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    }
-  }
-  /*{
-    instanceMethods:{
-        validatePassword:function(password){
-          return bcrypt.compareSync(password, this.password);
+    var User = sequelize.define('User', {
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: true,
+                validate: {
+                    isUnique: function (value, next) {
+                        var self = this;
+                        User.find({where: {email: value}})
+                            .then(function (user) {
+                                // reject if a different user wants to use the same email
+                                if (user && self.id !== user.id) {
+                                    return next('Email already in use!');
+                                }
+                                return next();
+                            })
+                            .catch(function (err) {
+                                return next(err);
+                            });
+                    }
+                }
+            },
+
+
+            /*email: {
+              type: DataTypes.STRING,
+              allowNull: false,
+              validate: {
+                isEmail:true
+              },
+              unique: {
+                args: true,
+                msg: 'Email address already in use!'
+              }
+            },*/
+
+            /*email: {
+              type: DataTypes.STRING,
+              allowNull: false,
+              unique: true
+            },*/
+            password: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            }
         }
-      }*/
-  );
+    );
 
-  //User.associate = (models) => {};
-
-  /*User.beforeCreate(function(user, options) {
-    return cryptPassword(user.password)
-      .then(success => {
-        user.password = success;
-      })
-      .catch(err => {
-        if (err) console.log(err);
-      });
-  });
-
-  function cryptPassword(password) {
-    console.log("cryptPassword" + password);
-    return new Promise(function(resolve, reject) {
-      bcrypt.genSalt(10, function(err, salt) {
-        // Encrypt password using bycrpt module
-        if (err) return reject(err);
-
-        bcrypt.hash(password, salt, null, function(err, hash) {
-          if (err) return reject(err);
-          return resolve(hash);
-        });
-      });
+    // encrypts the password before saving
+    User.beforeCreate((user, options) => {
+        return bcrypt.hash(user.password, 10)
+            .then(hash => {
+                user.password = hash;
+            })
+            .catch(err => {
+                throw new Error();
+            });
     });
-  }; */
+    // validates password
+    User.prototype.validatePassword = function (password) {
+        console.log(password);
+        return bcrypt.compareSync(password, this.password);
+    }
+    // override toJSON to hide the user password
+    User.prototype.toJSON = function () {
+        var values = Object.assign({}, this.get());
+        delete values.password;
+        return values;
+    }
 
-  User.beforeCreate((user, options) => {
-    return bcrypt.hash(user.password, 10)
-      .then(hash => {user.password = hash;})
-      .catch(err => { throw new Error(); });
-  });
-
-  User.prototype.validatePassword = function (password) {
-    console.log(password);
-    return bcrypt.compareSync(password, this.password);
-  }
-
-  return User;
+    return User;
 };
